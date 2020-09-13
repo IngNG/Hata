ffd7c1cc8f22b102393791efa6d5faf7091c8d94
Mikhail Abramov <beavisabra@yandex.ru>
2020-09-13 10:03:29 +0400

    Объединение

--- main.cpp
+++ main.cpp
@@ -1,33 +1,49 @@
 #include "TXLib.h"
+
+//ћожно две структуры дл€ раздела и подраздела
+struct BUTTON
+{
+
+};
+
 void fon(int mx, int my)
 {
+    //¬место 1000 везде лучше SCREEN_WIDTH какой-нибудь или txGetExtentX()
     txSetFillColor (TX_WHITE);
-    txRectangle (0, 0, 800, 100);
+    txRectangle (0, 0, 1000, 100);
 
+
+    //—делать функцией, завис€щей от раздела
     txSetColor (TX_BLACK, 4);
     if(txMouseX() >= 0 && txMouseX() <= 200 && txMouseY() >= 0 && txMouseY() <= 100)
         txSetColor (TX_LIGHTBLUE, 4);
-    txDrawText(0, 0, 200, 100 ,"название раздела");
+    txDrawText(0, 0, 200, 100 ,"ћебель");
+
+
     txSetColor (TX_BLACK, 4);
     if(txMouseX() >= 200 && txMouseX() <= 400 && txMouseY() >= 0 && txMouseY() <= 100)
         txSetColor (TX_LIGHTBLUE, 4);
-    txDrawText(200, 0, 400, 100 ,"название раздела");
+    txDrawText(200, 0, 400, 100 ,"техника");
     txSetColor (TX_BLACK, 4);
     if(txMouseX() >= 400 && txMouseX() <= 600 && txMouseY() >= 0 && txMouseY() <= 100)
         txSetColor (TX_LIGHTBLUE, 4);
-    txDrawText(400, 0, 600, 100 ,"название раздела");
+    txDrawText(400, 0, 600, 100 ,"пол");
     txSetColor (TX_BLACK, 4);
     if(txMouseX() >= 600 && txMouseX() <= 800 && txMouseY() >= 0 && txMouseY() <= 100)
         txSetColor (TX_LIGHTBLUE, 4);
-    txDrawText(600, 0, 800, 100 ,"название раздела");
+    txDrawText(600, 0, 800, 100 ,"комната");
+    if(txMouseX() >= 800 && txMouseX() <= 1000 && txMouseY() >= 0 && txMouseY() <= 100)
+        txSetColor (TX_LIGHTBLUE, 4);
+    txDrawText(800, 0, 1000, 100 ,"стены");
 
     txRectangle (mx, my, mx + 150, my + 140);
 }
+
 void openSubsection()
 {
     txSetColor (TX_BLACK, 4);
     txSetFillColor (TX_WHITE);
-    txRectangle (0, 0, 800, 100);
+    txRectangle (0, 0, 1000, 100);
     txSetColor (TX_BLACK, 2);
     if(txMouseX() >= 0 && txMouseX() <= 35 && txMouseY() >= 0 && txMouseY() <= 20)
         txSetColor (TX_LIGHTRED, 2);
@@ -37,70 +53,100 @@ void openSubsection()
 }
 
 int main()
-    {
-    txCreateWindow (800, 800);
+{
+    txCreateWindow (1000, 800);
+
+    HDC Kreslo = txLoadImage ("кресло.bmp");
+    HDC Stol = txLoadImage ("—тол.bmp");
+
+    bool drawKreslo = false;
+    bool drawstol = false;
 
     bool openSubsect = false;
     int mx = -500;
     int my = -500;
     char section [3][6][13];
-    sprintf(section [0][0], "0подраздел0") ;
-    sprintf(section [0][1], "0подраздел1") ;
-    sprintf(section [0][2], "0подраздел2") ;
-    sprintf(section [0][3], "0подраздел3") ;
-    sprintf(section [0][4], "0подраздел4") ;
+    sprintf(section [0][0], "комната") ;
+    sprintf(section [0][1], "стол") ;
+    sprintf(section [0][2], "мебель") ;
+    sprintf(section [0][3], "стуль€") ;
+    sprintf(section [0][4], "стол + комп") ;
     sprintf(section [0][5], "0подраздел5") ;
     sprintf(section [0][6], "0подраздел6") ;
 
     while(!GetAsyncKeyState(VK_ESCAPE))
     {
-    txSetColor (TX_WHITE, 4);
-    txClear();
-    txSetColor (TX_BLACK, 4);
+        txSetFillColor (TX_WHITE);
+        txClear();
+        txSetColor (TX_BLACK, 4);
 
-    if(!openSubsect)
-    {
-        fon(mx, my);
-        for(int i = 0; i < 7; i++)
+        if(!openSubsect)
         {
-            if(txMouseX() >= mx + 5 && txMouseX() <= mx + 150 && txMouseY() >= my + 5 + i * 20 && txMouseY() <= my + 20 + i * 20)
-                txSetColor (TX_LIGHTBLUE, 4);
-            txDrawText(mx, my + i * 20, mx + 150, my + 20 + i * 20 , section [0][i]);
-            if(txMouseX() >= mx + 5 && txMouseX() <= mx + 150 && txMouseY() >= my + 5 + i * 20 && txMouseY() <= my + 20 + i * 20 && txMouseButtons() == 1)
-                openSubsect = true;
-            txSetColor (TX_BLACK, 4);
+            fon(mx, my);
+            for(int i = 0; i < 7; i++)
+            {
+                if(txMouseX() >= mx + 5 && txMouseX() <= mx + 150 && txMouseY() >= my + 5 + i * 20 && txMouseY() <= my + 20 + i * 20)
+                    txSetColor (TX_LIGHTBLUE, 4);
+                txDrawText(mx, my + i * 20, mx + 150, my + 20 + i * 20 , section [0][i]);
+                if(txMouseX() >= mx + 5 && txMouseX() <= mx + 150 && txMouseY() >= my + 5 + i * 20 && txMouseY() <= my + 20 + i * 20 && txMouseButtons() == 1)
+                    openSubsect = true;
+                txSetColor (TX_BLACK, 4);
+            }
         }
-    }
 
-    else
-        openSubsection();
+        else
+            openSubsection();
 
-    if(txMouseX() >= 0 && txMouseX() <= 35 && txMouseY() >= 0 && txMouseY() <= 20 && txMouseButtons() == 1 && openSubsect)
-        {openSubsect = false; mx = -500; txSleep(50);}
+        if(txMouseX() >= 0 && txMouseX() <= 35 && txMouseY() >= 0 && txMouseY() <= 20 && txMouseButtons() == 1 && openSubsect)
+            {openSubsect = false; mx = -500; txSleep(50);}
 
-    for(int i = 0; i < 800; i = i + 200)
-    {
-        if(txMouseX() >= 0 + i && txMouseX() <= 200 + i && txMouseY() >= 0 && txMouseY() <= 100 && txMouseButtons() == 1)
+        for(int i = 0; i < 800; i = i + 200)
         {
-            my = txMouseY() ;
-            mx = txMouseX() ;
-            if(mx + 200 >= 800)
-               mx = 650;
+            if(txMouseX() >= 0 + i && txMouseX() <= 200 + i && txMouseY() >= 0 && txMouseY() <= 100 && txMouseButtons() == 1)
+            {
+                my = txMouseY() ;
+                mx = txMouseX() ;
+                if(mx + 200 >= 800)
+                   mx = 650;
+            }
         }
-    }
 
-    txSleep(10) ;
 
-    if(!(txMouseX() >= mx, txMouseY() >= my, txMouseX() <= mx + 200,txMouseY() <= my + 140) && txMouseButtons() == 1)
-    {
-        mx = -500;
-        my = -500;
-    }
+        //кресло
+        for(int i = 0; i < 800; i = i + 200)
+        {
+            if(i == 0 && txMouseX() >= 0 + i && txMouseX() <= 200 + i && txMouseY() >= 0 && txMouseY() <= 100 && txMouseButtons() == 1)
+            {
+                drawKreslo = true;
+            }
+            if(i == 1 && txMouseX() >= 0 + i && txMouseX() <= 200 + i && txMouseY() >= 0 && txMouseY() <= 100 && txMouseButtons() == 1)
+            {
+                drawKreslo = false;
+            }
+        }
 
-    txSleep(20) ;
+        //ј вот тут можно сообразить, как выгл€дит структура дл€ картинки
+        if (drawKreslo)
+        {
+            Win32::TransparentBlt (txDC(), 200, 200, 200, 200, Kreslo, 0, 0, 648, 612, TX_BLACK);
+        }
+        if (drawstol)
+        {
+            Win32::TransparentBlt (txDC(), 400, 400, 400, 400, Stol, 0, 0, 434, 258, TX_BLACK);
+        }
+
+
+
+        if(!(txMouseX() >= mx, txMouseY() >= my, txMouseX() <= mx + 200,txMouseY() <= my + 140) && txMouseButtons() == 1)
+        {
+            mx = -500;
+            my = -500;
+        }
+
+        txSleep(20) ;
 
     }
 
     return 0;
-    }
+}
 

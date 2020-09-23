e8a7a2a7270d21a418bbb0028f05146b73525f4f
NikitaSharow <71020822+NikitaSharow@users.noreply.github.com>
2020-09-23 17:03:43 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include "TXLib.h"
 
 //Можно две структуры для раздела и подраздела
+
+struct subBUTTON
+{
+    //int x;
+    //int y;
+    const char* text;
+};
+
 struct BUTTON
 {
-    int x;
-    int y;
-    char text[];
+    //int x;
+    //int y;
+    const char* text;
+    subBUTTON subButtons[10];
 };
 
 void drawButton(int x, int y, char text[] )
@@ -49,9 +58,11 @@ void fon(int mx, int my)
 
 void openSubsection()
 {
+    txSetColor (TX_WHITE, 4);
+    txClear();
     txSetColor (TX_BLACK, 4);
     txSetFillColor (TX_WHITE);
-    txRectangle (0, 0, 800, 100);
+    txRectangle (0, 0, 1000, 100);
     txSetColor (TX_BLACK, 2);
     if(txMouseX() >= 0 && txMouseX() <= 35 && txMouseY() >= 0 && txMouseY() <= 20)
         txSetColor (TX_LIGHTRED, 2);
@@ -62,7 +73,7 @@ void openSubsection()
 
 int main()
 {
-    txCreateWindow (1080, 800);
+    txCreateWindow (1000, 800);
 
 
     bool openSubsect = false;
@@ -71,6 +82,15 @@ int main()
     object[0] = {50, 0, txLoadImage ("Pictures/кресло.bmp"), false, 686, 700};
     object[1] = {150, 0, txLoadImage ("Pictures/Стол.bmp"), false, 910, 746};
     object[2] = {250, 0, txLoadImage ("Pictures/кресло2.bmp"), false, 822, 836};
+
+    subBUTTON btn[10];
+    btn[0] = {"комната"};
+    btn[1] = {"стол"};
+    btn[2] = {"Мебель"};
+    btn[3] = {"стулья"};
+    btn[4] = {"Мебель"};
+    BUTTON buttons[5];
+    buttons[0] = {"Мебель",        btn    };
 
     bool drawKresloB2=false;
     //HDC Sofa2 = txLoadImage("Pictures/диван.bmp");
@@ -81,7 +101,7 @@ int main()
     int window = 0;
     int mx = -500;
     int my = -500;
-    char section [3][6][13];
+    char section [3][6][12];
     sprintf(section [0][0], "комната") ;
     sprintf(section [0][1], "стол") ;
     sprintf(section [0][2], "мебель") ;
@@ -89,7 +109,6 @@ int main()
     sprintf(section [0][4], "стол + комп") ;
     sprintf(section [0][5], "0подраздел5") ;
     sprintf(section [0][6], "0подраздел6") ;
-
 
     txBegin();
     while(!GetAsyncKeyState(VK_ESCAPE))
@@ -102,16 +121,16 @@ int main()
         {
             fon(mx, my);
             txRectangle (mx, my, mx + 150, my + 140);
-            for(int i = 0; i < 7; i++)
+            for(int i = 0; i < 4; i++)
             {
                 if(txMouseX() >= mx + 5 && txMouseX() <= mx + 150 && txMouseY() >= my + 5 + i * 20 && txMouseY() <= my + 20 + i * 20)
                     txSetColor (TX_LIGHTBLUE, 4);
-                txDrawText(mx, my + i * 20, mx + 150, my + 20 + i * 20 , section [0][i]);
+                txDrawText(mx, my + i * 20, mx + 150, my + 20 + i * 20 , buttons[0].subButtons[i]);
                 if(txMouseX() >= mx + 5 && txMouseX() <= mx + 150 && txMouseY() >= my + 5 + i * 20 && txMouseY() <= my + 20 + i * 20 && txMouseButtons() == 1)
                     openSubsect = true;
                 txSetColor (TX_BLACK, 4);
             }
-            txSleep(200);
+            txSleep(20);
         }
 
         else
@@ -121,14 +140,14 @@ int main()
             {openSubsect = false; mx = -500; txSleep(200);}
 
 
-        for(int i = 0; i < 800; i = i + 200)
+        for(int i = 0; i < 1000; i = i + 200)
         {
             if(txMouseX() >= 0 + i && txMouseX() <= 200 + i && txMouseY() >= 0 && txMouseY() <= 100 && txMouseButtons() == 1)
             {
                 my = txMouseY() ;
                 mx = txMouseX() ;
-                if(mx + 200 >= 800)
-                   mx = 650;
+                if(mx + 150 >= 1000)
+                   mx = 850;
             }
         }
 
@@ -155,7 +174,7 @@ int main()
         for(int i = 0; i < 3; i++)
             if (object[i].drawObject)
             {
-                Win32::TransparentBlt (txDC(),400,400,150,150,object[i].pic,0,0,object[i].width,object[i].height,TX_BLACK);
+                Win32::TransparentBlt (txDC(),object[i].x,object[i].y,150,150,object[i].pic,0,0,object[i].width,object[i].height,TX_BLACK);
             }
 
             //if (drawstol)
